b93bac0f2f9b80f125a988b62bd37e4a88a6409a
GabrielSilva13 <86579343+GabrielSilva13@users.noreply.github.com>
2021-11-21 10:39:04 -0300

    Add files via upload

---
+++ precache-manifest.da64aee82b72571ebdd66902171a583a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "03e68bb6fa7a3aec9bde813ca84f9afc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "57f403c5830997a0730f",
+    "url": "/static/css/main.01f38ec9.chunk.css"
+  },
+  {
+    "revision": "cf394c00e5dd9b625767",
+    "url": "/static/js/2.23c16226.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/static/js/2.23c16226.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "57f403c5830997a0730f",
+    "url": "/static/js/main.383ad7f9.chunk.js"
+  },
+  {
+    "revision": "6fc4f2c3fd5ae8d05fd1",
+    "url": "/static/js/runtime-main.4ba1596e.js"
+  }
+]);
